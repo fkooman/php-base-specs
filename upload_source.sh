0762da35cf1feb834a1791697a1604a46d505209
François Kooman <fkooman@tuxed.net>
2015-09-04 14:22:02 +0200

    add upload_source.sh script

---
+++ upload_source.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PACKAGE=$1
+
+rpmdev-wipetree
+VERSION=`cat $1.spec | grep "Version:" | cut -d ':' -f 2 | tr -d "[:space:]"`
+GH_NAME=`cat $1.spec | grep "%global github_name" | awk {'print $3'}`
+echo $GH_NAME
+
+(
+cp $1* ~/rpmbuild/SOURCES/
+mv ~/rpmbuild/SOURCES/$1.spec ~/rpmbuild/SPECS/
+cd ~/rpmbuild/SOURCES
+curl -O -L https://github.com/fkooman/$GH_NAME/archive/$VERSION.tar.gz
+cd ~/rpmbuild/SPECS/
+rpmbuild -bs $1.spec
+scp ~/rpmbuild/SPECS/*.spec ~/rpmbuild/SRPMS/*.rpm fedorapeople.org:~/public_html/$1/
+)
+
