aa7a5b23c97d90d211f748017d3848ca6b953b2d
François Kooman <fkooman@tuxed.net>
2016-03-18 18:12:05 +0000

    add make_all_source_rpm

---
+++ make_all_source_rpm.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+sh make_source_rpm.sh php-fkooman-base64
+sh make_source_rpm.sh php-fkooman-config
+sh make_source_rpm.sh php-fkooman-http
+sh make_source_rpm.sh php-fkooman-io
+sh make_source_rpm.sh php-fkooman-json
+sh make_source_rpm.sh php-fkooman-rest-plugin-authentication-basic
+sh make_source_rpm.sh php-fkooman-rest-plugin-authentication-bearer
+sh make_source_rpm.sh php-fkooman-rest-plugin-authentication-form
+sh make_source_rpm.sh php-fkooman-rest-plugin-authentication-mellon
+sh make_source_rpm.sh php-fkooman-rest-plugin-authentication
+sh make_source_rpm.sh php-fkooman-rest-plugin-authentication-tls
+sh make_source_rpm.sh php-fkooman-rest
+sh make_source_rpm.sh php-fkooman-tpl
+sh make_source_rpm.sh php-fkooman-tpl-twig
